d5da4ee3fceaeb2c6d72bd384c4675c36c9ffb13
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2023-02-14 21:43:44 +0100

    feat(docker): support build of docker images

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"archive/zip"
+	"bufio"
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -9,13 +12,28 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
 	cp "github.com/otiai10/copy"
 )
 
 const (
 	_sep = os.PathSeparator
 )
+
+var dockerImagePrefix = "bonitasoft/bonita-package-docker-"
+
+type ErrorLine struct {
+	Error       string      `json:"error"`
+	ErrorDetail ErrorDetail `json:"errorDetail"`
+}
+
+type ErrorDetail struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	// Try to find a Bonita zip file inside :
@@ -25,8 +43,8 @@ func main() {
 	}
 	if len(matches) == 0 || !Exists(filepath.Join("src", "my-application")) {
 		fmt.Println("Please place in src/ folder:")
-		fmt.Println(" * ZIP file of Bonita Tomcat Bundle (Eg. BonitaCommunity-2023.1.zip)")
-		fmt.Println(" * my-application/ folder containing all artifacts of your application")
+		fmt.Println(" * ZIP file of Bonita Tomcat Bundle (Eg. BonitaCommunity-2023.1-u0.zip, BonitaSubscription-2023.1-u2.zip)")
+		fmt.Println(" * my-application/ folder containing all artifacts of your application, or containing directly the .zip file of your application")
 		fmt.Println("and then re-run this program")
 		return
 	}
@@ -57,6 +75,82 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Done. Self-contained application available:", filepath.Join("output", bundleName+"-application.zip"))
+
+	fmt.Println("Building Docker images (Community & Subscription editions)")
+	buildDockerImages()
+}
+
+func buildDockerImages() {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	err = imageBuild(cli, "community")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	err = imageBuild(cli, "subscription")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func imageBuild(dockerClient *client.Client, edition string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
+	defer cancel()
+
+	tar, err := archive.TarWithOptions("src/", &archive.TarOptions{})
+	if err != nil {
+		return err
+	}
+
+	fullDockerImageName := dockerImagePrefix + edition
+	opts := types.ImageBuildOptions{
+		Dockerfile: "Dockerfile." + edition,
+		Tags:       []string{fullDockerImageName},
+		Remove:     true,
+	}
+	res, err := dockerClient.ImageBuild(ctx, tar, opts)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	err = print(res.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("\nSuccessfully created Docker image '%s'\n\n", fullDockerImageName)
+
+	return nil
+}
+
+func print(rd io.Reader) error {
+	var lastLine string
+
+	scanner := bufio.NewScanner(rd)
+	for scanner.Scan() {
+		lastLine = scanner.Text()
+		// FIXME: add -v --verbose parameter to activate this Println:
+		// fmt.Println(scanner.Text())
+	}
+
+	errLine := &ErrorLine{}
+	json.Unmarshal([]byte(lastLine), errLine)
+	if errLine.Error != "" {
+		return errors.New(errLine.Error)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func unzipFile(zipFile string, outputDir string) {
